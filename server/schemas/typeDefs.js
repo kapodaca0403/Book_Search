dc69937455bacdc8e6de8cf229dd2def96088acf
katerz avocado <ktrinaapodaca@yahoo.com>
2021-10-09 18:58:34 -0600

    fixing

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -21,7 +21,7 @@ const typeDefs = gql`
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    savedBooks(book: saveBook): User
+    savedBooks(book: SaveBook): User
     removeBook(bookID: String): User
   }
   input SaveBook {
